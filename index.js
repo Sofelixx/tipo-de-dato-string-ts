bae4b7a08691fda17f6dfc5a15ba32b060b6bd60
sofelix <s18paris@gmail.com>
2019-10-01 20:59:28 -0300

    se probo el tipo de dato string

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 "use strict";
+var batman = "Batman";
+var superman = "Superman";
+var linternaVerde = "Linterna Verde";
+console.log(batman);
+console.log(superman);
+console.log(linternaVerde);
+var concatenacion = "Los heroes son: " + batman + ", " + superman + " y " + linternaVerde;
+console.log(concatenacion);
--- index.ts
+++ index.ts
@@ -1,1 +1,8 @@
-
+let batman:string = "Batman";
+let superman:string = "Superman";
+let linternaVerde: string = "Linterna Verde";
+console.log(batman);
+console.log(superman);
+console.log(linternaVerde);
+let concatenacion:string = `Los heroes son: ${batman}, ${superman} y ${linternaVerde}`;
+console.log(concatenacion);
